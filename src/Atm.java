b64548b7e73982e045c5aa4967570394a04a7bc0
forem <forem999@mail.ru>
2021-12-15 21:06:38 +0300

    4

--- src/Atm.java
+++ src/Atm.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -27,10 +29,11 @@ public class Atm {
         for (Map.Entry<Integer, Integer> k: this.atmStore.entrySet()) {
             summ += k.getKey() * k.getValue();
         }
+        System.out.println(this.atmStore);
         return summ;
     }
 
-    public void putMoney (HashMap<Integer, Integer> incoming) {
+    private void putMoney (HashMap<Integer, Integer> incoming) {
         int[] flags = this.checkCells(incoming);
 
         if (Arrays.stream(flags).sum() > 0 ) {
@@ -42,7 +45,7 @@ public class Atm {
         }
     }
 
-    public void getMoney(HashMap<Integer, Integer> outgoing) {
+    private void getMoney(HashMap<Integer, Integer> outgoing) {
         int[] flags = this.checkCells(outgoing);
 
         if (Arrays.stream(flags).sum() < 0) {
@@ -69,18 +72,45 @@ public class Atm {
         return flags;
     }
 
-    public HashMap<Integer, Integer> inputParcer(String s) {
+    private HashMap<Integer, Integer> inputParser(String s) {
         HashMap<Integer, Integer> result = new HashMap<>();
         for (int k : keys) {
             result.put(k, 0);
         }
         String[] strArr = s.split(" ");
         Integer value = 0;
-        for (String l : strArr) {
-            value = Integer.parseInt(l);
+        if (strArr.length > 1) {
+            for (String l : strArr) {
+                value = Integer.parseInt(l);
+                result.put(value, result.get(value) + 1);
+            }
+        } else if (strArr.length == 1) {
+            value = Integer.parseInt(strArr[0]);
+            int left = 0;
+            int right = value;
             System.out.println(value);
+            for (int i = 6; i > 0; i--) {
+                left = right / keys[i];
+                right = right % keys[i];
+                result.put(keys[i], left);
+            }
         }
         return  result;
+    }
 
+    public void moneyReceiver () throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Внесение банкнот. Введите банкноты через пробел: ");
+        String inputString = reader.readLine();
+        HashMap<Integer, Integer> incoming = inputParser(inputString);
+        putMoney(incoming);
+    }
+
+    public void moneyGiver () throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Выдача банкнот. Введите сумму кратную 50: ");
+        String inputString = reader.readLine();
+        HashMap<Integer, Integer> incoming = inputParser(inputString);
+        getMoney(incoming);
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -9,31 +9,10 @@ public class Solution {
 
         Atm atm = new Atm(1);
         System.out.println(atm.getTotal());
-
-        HashMap<Integer, Integer> incoming = new HashMap<>();
-        incoming.put(50, 222);
-        incoming.put(100, 222);
-        incoming.put(200, 222);
-        incoming.put(500, 222);
-        incoming.put(1000, 222);
-        incoming.put(2000, 222);
-        incoming.put(5000, 222);
-
-        atm.putMoney(incoming);
-
+//        atm.moneyReceiver();
+//        System.out.println(atm.getTotal());
+        atm.moneyGiver();
         System.out.println(atm.getTotal());
 
-        atm.getMoney(incoming);
-
-        System.out.println(atm.getTotal());
-
-        atm.getMoney(incoming);
-
-        System.out.println(atm.getTotal());
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Введите банкноты через пробел: 1");
-        String str = reader.readLine();
-        atm.inputParcer(str);
     }
 }
