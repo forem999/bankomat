8d09ebc71f4a1e5fbbefa6f97fad0c706ceb668a
forem <forem999@mail.ru>
2021-12-15 18:01:44 +0300

    4

--- src/Atm.java
+++ src/Atm.java
@@ -4,10 +4,15 @@ import java.util.Map;
 
 public class Atm {
     static int count = 0;
+    //размер ячейки для банкноты;
     private int maxCellValue = 333;
+    // массив с номиналами банкнот, и ключи для словаря банкомата
     private int[] keys = {50,100,200,500,1000,2000,5000};
+    // массив с дефалтным размером ячейки, используется для конструктора банкомата;
     private int[] defValues = {222,222,222,222,222,222,222};
+    // индекс банкомата, порядковый номер;
     public  int index;
+    // словарь банкомата;
     public  HashMap<Integer, Integer> atmStore = new HashMap<>();
 
     public Atm (int index) {
@@ -26,7 +31,7 @@ public class Atm {
     }
 
     public void putMoney (HashMap<Integer, Integer> incoming) {
-        int[] flags = this.check(incoming);
+        int[] flags = this.checkCells(incoming);
 
         if (Arrays.stream(flags).sum() > 0 ) {
             System.out.println("Внесение денег невозможно. Ячейки будут переполнены.");
@@ -38,7 +43,7 @@ public class Atm {
     }
 
     public void getMoney(HashMap<Integer, Integer> outgoing) {
-        int[] flags = this.check(outgoing);
+        int[] flags = this.checkCells(outgoing);
 
         if (Arrays.stream(flags).sum() < 0) {
             System.out.println("Выдача денег невозможна. Не хватает необходимых банкнот.");
@@ -49,7 +54,7 @@ public class Atm {
         }
     }
 
-    public int[] check (HashMap<Integer, Integer> map) {
+    private int[] checkCells (HashMap<Integer, Integer> map) {
         int[] flags = {0, 0, 0, 0, 0, 0, 0};
         int c = 0;
         for (int i : keys) {
@@ -62,5 +67,20 @@ public class Atm {
             count++;
         }
         return flags;
+    }
+
+    public HashMap<Integer, Integer> inputParcer(String s) {
+        HashMap<Integer, Integer> result = new HashMap<>();
+        for (int k : keys) {
+            result.put(k, 0);
+        }
+        String[] strArr = s.split(" ");
+        Integer value = 0;
+        for (String l : strArr) {
+            value = Integer.parseInt(l);
+            System.out.println(value);
+        }
+        return  result;
+
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,9 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 
 public class Solution {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         Atm atm = new Atm(1);
         System.out.println(atm.getTotal());
@@ -24,5 +26,14 @@ public class Solution {
         atm.getMoney(incoming);
 
         System.out.println(atm.getTotal());
+
+        atm.getMoney(incoming);
+
+        System.out.println(atm.getTotal());
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Введите банкноты через пробел: 1");
+        String str = reader.readLine();
+        atm.inputParcer(str);
     }
 }
